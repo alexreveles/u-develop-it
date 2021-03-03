4abb82d16d0ee8d29ee73a0d06ddaede7f286f00
alex <alexreveles@yahooo.com>
2021-03-02 20:47:56 -0800

    connected to expres.js

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+// Test the Express.js
+// app.get('/', (req, res) => {
+//     res.json({
+//       message: 'Hello World'
+//     });
+//   });
+
+// Default response for any other request(Not Found) Catch all
+app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+
+
+
+
+
+// Function that will start the Express.js server on port 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+  
